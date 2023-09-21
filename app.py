310f8d25f809c64d7b1f2e85e9d8d9b8c52f6b03
Jeydev <insaneonai@gmail.com>
2023-09-21 21:00:27 +0530

    SIH-2023
    1st commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+
+
+def translate(english_text,url="https://f9f4-34-27-215-117.ngrok-free.app/translate"):
+    st.session_state["hindi"] = requests.get(url,params={"English_text":english_text}).json()["Hindi_text"]
+
+
+st.header("ByteLinguists Translation app")
+
+st.text_area("English Text", key="english", placeholder="Please enter text in English")
+st.text_area("Hindi Text", key="hindi", placeholder="Please enter text in Hindi",disabled=True)
+st.button('Translate ⚡', on_click=translate, args=[st.session_state['english']])
